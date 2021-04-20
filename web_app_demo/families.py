3c15f3d67aaefd655f0856b5adcf85e4c9b65724
VianneyMI <vianney.mixtur@outlook.fr>
2021-04-20 17:35:01 -0400

    Final touch

---
+++ web_app_demo/families.py
@@ -0,0 +1,240 @@
+"""
+This is the families module and supports all the REST actions for the
+families data
+"""
+
+import json
+from flask import make_response, abort
+from config import db
+from models import Family, FamilySchema, Material, MaterialSchema
+
+
+def read_all():
+    """
+    This function responds to a request for /api/families
+    with the complete lists of families
+
+    :return:        json string of list of families
+    """
+    # Create the list of families from our data
+    families = Family.query.order_by(Family.family_name).all()
+
+    # Serialize the data for the response
+    family_schema = FamilySchema(many=True)
+    data = family_schema.dump(families).data
+
+    return data
+
+
+def read_one(family_id):
+    """
+    This function responds to a request for /api/families/{family_id}
+    with one matching family from families
+
+    :param family_id:   Id of family to find
+    :return:            family matching id
+    """
+    # Build the initial query
+    family = (
+        Family.query.filter(Family.family_id == family_id)
+        .outerjoin(Material)
+        .one_or_none()
+    )
+
+    # Did we find a family?
+    if family is not None:
+
+        # Serialize the data for the response
+        family_schema = FamilySchema()
+        data = family_schema.dump(family).data
+        return data
+
+    # Otherwise, nope, didn't find that family
+    else:
+        abort(404, f"Family not found for Id: {family_id}")
+
+
+def get_nb_materials(family_id):
+    # Build the initial query
+    family = (
+        Family.query.filter(Family.family_id == family_id)
+        .outerjoin(Material)
+        .one_or_none()
+    )
+
+    # Did we find a family?
+    if family is not None:
+
+        # Serialize the data for the response
+        #family_schema = FamilySchema()
+        #data = family_schema.dump(family).data
+        return len(family.materials)
+
+    # Otherwise, nope, didn't find that family
+    else:
+        abort(404, f"Family not found for Id: {family_id}")
+
+def get_min_ms(family_id):
+    material = (
+        Material.query.filter(Material.family_id == family_id)
+        .outerjoin(Family)
+        .order_by(Material.masse_surfacique)
+        .first()
+    )
+    if material is not None:
+
+        return material.masse_surfacique
+    else:
+
+        abort(404, f"Family not found for Id: {family_id}")
+
+def get_max_ms(family_id):
+    material = (
+        Material.query.filter(Material.family_id == family_id)
+        .outerjoin(Family)
+        .order_by(db.desc(Material.masse_surfacique))
+        .first()
+    )
+    if material is not None:
+
+        return material.masse_surfacique
+    else:
+
+        abort(404, f"Family not found for Id: {family_id}")
+
+def get_min_mc(family_id):
+    material = (
+        Material.query.filter(Material.family_id == family_id)
+        .outerjoin(Family)
+        .order_by(Material.masse_combustible)
+        .first()
+    )
+    if material is not None:
+
+        return material.masse_combustible
+    else:
+
+        abort(404, f"Family not found for Id: {family_id}")
+
+def get_max_mc(family_id):
+    material = (
+        Material.query.filter(Material.family_id == family_id)
+        .outerjoin(Family)
+        .order_by(db.desc(Material.masse_combustible))
+        .first()
+    )
+    if material is not None:
+
+        return material.masse_combustible
+    else:
+
+        abort(404, f"Family not found for Id: {family_id}")
+
+
+def create(family):
+    """
+    This function creates a new family in the families structure
+    based on the passed in family data
+
+    :param family:  family to create in families structure
+    :return:        201 on success, 406 on family exists
+    """
+    family_name = family.get("family_name")
+
+
+    existing_family = (
+        Family.query.filter(Family.family_name == family_name)
+        .one_or_none()
+    )
+
+    # Can we insert this family?
+    if existing_family is None:
+
+        # Create a family instance using the schema and the passed in family
+        schema = FamilySchema()
+        new_family = schema.load(family, session=db.session).data
+
+        # Add the family to the database
+        db.session.add(new_family)
+        db.session.commit()
+
+        # Serialize and return the newly created family in the response
+        data = schema.dump(new_family).data
+
+        return data, 201
+
+    # Otherwise, nope, family exists already
+    else:
+        abort(409, f"Family {family_name}  exists already")
+
+
+def update(family_id, family):
+    """
+    This function updates an existing family in the families structure
+
+    :param family_id:   Id of the family to update in the families structure
+    :param family:      family to update
+    :return:            updated family structure
+    """
+    # Get the family requested from the db into session
+    update_family = Family.query.filter(
+        Family.family_id == family_id
+    ).one_or_none()
+
+    # Did we find an existing family?
+    if update_family is not None:
+
+        # turn the passed in family into a db object
+        schema = FamilySchema()
+        update = schema.load(family, session=db.session).data
+
+        # Set the id to the family we want to update
+        update.family_id = update_family.family_id
+
+        # merge the new object into the old and commit it to the db
+        db.session.merge(update)
+        db.session.commit()
+
+        # return updated family in the response
+        data = schema.dump(update_family).data
+
+        return data, 200
+
+    # Otherwise, nope, didn't find that family
+    else:
+        abort(404, f"Family not found for Id: {family_id}")
+
+def calculate(family_id):
+    nb_materials = get_nb_materials(family_id)
+    min_ms = get_min_ms(family_id)
+    max_ms = get_max_ms(family_id)
+    min_mc = get_min_mc(family_id)
+    max_mc = get_max_mc(family_id)
+
+    return ({
+        'nb_materials':nb_materials,
+        'min_ms':min_ms,
+        'max_ms':max_ms,
+        'min_mc':min_mc,
+        'max_mc':max_mc
+    })
+
+def delete(family_id):
+    """
+    This function deletes a family from the families structure
+
+    :param family_id:   Id of the family to delete
+    :return:            200 on successful delete, 404 if not found
+    """
+    # Get the family requested
+    family = Family.query.filter(Family.family_id == family_id).one_or_none()
+
+    # Did we find a family?
+    if family is not None:
+        db.session.delete(family)
+        db.session.commit()
+        return make_response(f"Family {family_id} deleted", 200)
+
+    # Otherwise, nope, didn't find that family
+    else:
+        abort(404, f"Family not found for Id: {family_id}")
---
+++ web_app_demo/static/js/materials.js
@@ -0,0 +1,327 @@
+/*
+ * JavaScript file for the application to demonstrate
+ * using the API for creating, updating and deleting materials
+ */
+
+// Create the namespace instance
+let ns = {};
+
+// Create the model instance
+ns.model = (function () {
+    'use strict';
+
+    // Return the API
+    return {
+        read_one: function (family_id, material_id) {
+            let ajax_options = {
+                type: 'GET',
+                url: `/api/families/${family_id}/materials/${material_id}`,
+                accepts: 'application/json',
+                dataType: 'json'
+            };
+            return $.ajax(ajax_options);
+        },
+        read: function (family_id) {
+            let ajax_options = {
+                type: 'GET',
+                url: `/api/families/${family_id}`,
+                accepts: 'application/json',
+                dataType: 'json'
+            };
+            return $.ajax(ajax_options);
+        },
+        create: function (family_id, material) { //material should be an object then ...
+            let ajax_options = {
+                type: 'POST',
+                url: `/api/families/${family_id}/materials`,
+                accepts: 'application/json',
+                contentType: 'application/json',
+                dataType: 'json',
+                data: JSON.stringify(material) //data is material object
+            };
+
+            return $.ajax(ajax_options);
+        },
+        update: function (family_id, material) {
+            let ajax_options = {
+                type: 'PUT',
+                url: `/api/families/${family_id}/materials/${material.material_id}`,
+                accepts: 'application/json',
+                contentType: 'application/json',
+                dataType: 'json',
+                data: JSON.stringify(material)
+            };
+            return $.ajax(ajax_options);
+        },
+        'delete': function (family_id, material_id) {
+            let ajax_options = {
+                type: 'DELETE',
+                url: `/api/families/${family_id}/materials/${material_id}`,
+                accepts: 'application/json',
+                contentType: 'plain/text'
+            };
+            return $.ajax(ajax_options);
+        }
+    };
+}());
+
+// Create the view instance
+ns.view = (function () {
+    'use strict';
+
+    const NEW_MATERIAL = 0,
+        EXISTING_MATERIAL = 1;
+
+    let $family_id = $('#family_id'),
+        $family_name = $('#family_name'),
+        $timestamp = $('#timestamp'),
+        $material_id = $('#material_id'),
+        $masse_combustible = $('#masse_combustible'),
+        $masse_surfacique = $('#masse_surfacique'),
+        $material_name = $('#material_name'),
+        $material = $('#material'),
+        $create = $('#create'),
+        $update = $('#update'),
+        $delete = $('#delete'),
+        $reset = $('#reset');
+
+    // return the API
+    return {
+        NEW_MATERIAL: NEW_MATERIAL,
+        EXISTING_MATERIAL: EXISTING_MATERIAL,
+        reset: function () {
+            $material_id.text('');
+            $material_name.val('');
+            $masse_surfacique.val('');
+            $masse_combustible.val('');
+            $material.val('').focus();
+        },
+        update_editor: function (material) {
+            $material_id.text(material.material_id);
+            $material_name.val(material.material_name);
+            $masse_surfacique.val(material.masse_surfacique);
+            $masse_combustible.val(material.masse_combustible);
+            $material.val(material.material_name).focus();
+        },
+        set_button_states: function (state) {
+            if (state === NEW_MATERIAL) {
+                $create.prop('disabled', false);
+                $update.prop('disabled', true);
+                $delete.prop('disabled', true);
+            } else if (state === EXISTING_MATERIAL) {
+                $create.prop('disabled', true);
+                $update.prop('disabled', false);
+                $delete.prop('disabled', false);
+            }
+        },
+        build_table: function (family) {
+            let source = $('#materials-table-template').html(),
+                template = Handlebars.compile(source),
+                html;
+
+            // update the family data
+            $family_id.text(family.family_id);
+            $family_name.text(family.family_name);
+            $timestamp.text(family.timestamp);
+
+            // clear the table
+            $('.materials table > tbody').empty();
+
+            // did we get a material array?
+            if (family.materials) {
+
+                // Create the HTML from the template and materials
+                html = template({materials: family.materials});
+
+                // Append the html to the table
+                $('table').append(html);
+            }
+        },
+        error: function (error_msg) {
+            $('.error')
+                .text(error_msg)
+                .css('visibility', 'visible');
+            setTimeout(function () {
+                $('.error').fadeOut();
+            }, 2000)
+        }
+    };
+}());
+
+// Create the controller
+ns.controller = (function (m, v) {
+    'use strict';
+
+    let model = m,
+        view = v,
+        $url_family_id = $('#url_family_id'),
+        $url_material_id = $('#url_material_id'),
+        $material_id = $('#material_id'),
+        $material_name = $('#material_name'),
+        $masse_surfacique = $('#masse_surfacique'),
+        $masse_combustible = $('#masse_combustible'),
+        $material = $('#material');
+
+    // read the family data with materials
+    setTimeout(function () {
+        view.reset();
+        model.read(parseInt($url_family_id.val()))
+            .done(function (data) {
+                view.build_table(data);
+                view.update_editor(data);
+                view.set_button_states(view.NEW_MATERIAL);
+            })
+            .fail(function (xhr, textStatus, errorThrown) {
+                error_handler(xhr, textStatus, errorThrown);
+            });
+
+        if ($url_material_id.val() !== "") {
+            model.read_one(parseInt($url_family_id.val()), parseInt($url_material_id.val()))
+                .done(function (data) {
+                    view.update_editor(data);
+                    view.set_button_states(view.EXISTING_MATERIAL);
+                })
+                .fail(function (xhr, textStatus, errorThrown) {
+                    error_handler(xhr, textStatus, errorThrown);
+                });
+        }
+    }, 100);
+
+    // generic error handler
+    function error_handler(xhr, textStatus, errorThrown) {
+        let error_msg = `${textStatus}: ${errorThrown} - ${xhr.responseJSON.detail}`;
+
+        view.error(error_msg);
+        console.log(error_msg);
+    }
+
+    // initialize the button states
+    view.set_button_states(view.NEW_MATERIAL);
+
+    // Validate input
+    function validate(material_name, masse_surfacique, masse_combustible) {
+        return material_name !== "" && masse_surfacique!="" && masse_combustible!="";
+    }
+
+    // Create our event handlers
+    $('#create').click(function (e) {
+        let material_name = $material_name.val(),
+            masse_surfacique = parseInt($masse_surfacique.val(),10),
+            masse_combustible = parseInt($masse_combustible.val(),10);
+
+
+
+        e.preventDefault();
+
+        if (validate(material_name, masse_surfacique, masse_combustible)) {
+            console.log(masse_surfacique);
+            console.log(masse_combustible);
+            model.create(parseInt($('#url_family_id').val()),{
+                'family_id':parseInt($('#url_family_id').val()),
+                'material_name':material_name,
+                'masse_surfacique':masse_surfacique,
+                'masse_combustible':masse_combustible
+            })
+                .done(function (data) {
+                    model.read(parseInt($('#url_family_id').val()))
+                        .done(function(data) {
+                            view.build_table(data);
+                        })
+                        .fail(function(xhr, textStatus, errorThrown) {
+                            error_handler(xhr, textStatus, errorThrown);
+                        });
+                    view.reset();
+                    view.set_button_states(view.NEW_MATERIAL);
+                })
+                .fail(function (xhr, textStatus, errorThrown) {
+                    error_handler(xhr, textStatus, errorThrown);
+                });
+
+        } else {
+            alert('Problem with material input');
+        }
+    });
+
+    $('#update').click(function (e) {
+        let family_id = parseInt($url_family_id.val()),
+            material_id = parseInt($material_id.text()),
+            material = $material.val();
+
+        e.preventDefault();
+
+        if (validate(material_name, masse_surfacique, masse_combustible)) {
+            model.update(family_id, {
+                'material_id': material_id,
+                'material_name': material_name,
+                'masse_surfacique':masse_surfacique,
+                'masse_combustible':$masse_combustible
+            })
+                .done(function (data) {
+                    model.read(data.family.family_id)
+                        .done(function(data) {
+                            view.build_table(data);
+                        })
+                        .fail(function(xhr, textStatus, errorThrown) {
+                            error_handler(xhr, textStatus, errorThrown);
+                        });
+                    view.reset();
+                    view.set_button_states(view.NEW_MATERIAL);
+                })
+                .fail(function (xhr, textStatus, errorThrown) {
+                    error_handler(xhr, textStatus, errorThrown);
+                });
+
+        } else {
+            alert('Problem with family name');
+        }
+    });
+
+    $('#delete').click(function (e) {
+        let family_id = parseInt($url_family_id.val()),
+            material_id = parseInt($material_id.text());
+
+        e.preventDefault();
+
+        if (validate('placeholder', family_name)) {
+            model.delete(family_id, material_id)
+                .done(function (data) {
+                    model.read(parseInt($('#url_family_id').val()))
+                        .done(function(data) {
+                            view.build_table(data);
+                        })
+                        .fail(function(xhr, textStatus, errorThrown) {
+                            error_handler(xhr, textStatus, errorThrown);
+                        });
+                    view.reset();
+                    view.set_button_states(view.NEW_MATERIAL);
+                })
+                .fail(function (xhr, textStatus, errorThrown) {
+                    error_handler(xhr, textStatus, errorThrown);
+                });
+
+        } else {
+            alert('Problem with family name');
+        }
+    });
+
+    $('#reset').click(function () {
+        view.reset();
+        view.set_button_states(view.NEW_MATERIAL);
+    })
+
+    $('table').on('click', 'tbody tr', function (e) {
+        let $target = $(e.target).parent(),
+            material_id = $target.data('material_id'),
+            material_name = $target.data('material_name'),
+            masse_surfacique = $target.data('masse_surfacique'),
+            masse_combustible = $target.data('masse_combustible');
+
+        view.update_editor({
+            material_id: material_id,
+            material_name: material_name,
+            masse_surfacique: masse_surfacique,
+            masse_combustible: masse_combustible,
+        });
+        view.set_button_states(view.EXISTING_MATERIAL);
+    });
+}(ns.model, ns.view));
